2e6fa7dfb9278a50da53ffc9435f26d2b40277ea
justSWAYAM <just.swayammishra@gmail.com>
2024-10-05 18:43:18 +0530

    env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,12 @@ export const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDQyZTZkYTU2YzhjZGU5NTdkNDA5ODkzZjU0OTBmOCIsIm5iZiI6MTcyNzc4ODAxMy44NjE2OTgsInN1YiI6IjY2ZmJmMzIyOGFhNzM0YjM3NmE2N2EwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gv8tfXW3FFRiHcd6rpBYGeRXnAMwEoDTdL40zLXBlec'
+      Authorization: 'Bearer ' + process.env.REACT_APP_TMDB_KEY,
     }
   };
 
   export const IMG = "https://image.tmdb.org/t/p/w500/";
 
-  export const OPEN_AI = "AIzaSyDSmrh1W3Voq6QqcuM4TYnLK-_jQ-N1XWQ";
+  export const OPEN_AI = process.env.REACT_APP_OPENAI_KEY;
+
+ 
